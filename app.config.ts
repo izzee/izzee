743d39afa326d4cd08454c74b1575f15ded50acb
Isaac Chabon <ichabon@me.com>
2025-01-26 15:55:45 -0500

    set up feed

---
+++ app.config.ts
@@ -0,0 +1,6 @@
+export default defineAppConfig({
+  info: {
+    title: "izz.ee",
+    site_url: "deploy-preview-2--izzee.netlify.app"
+  }
+})
---
+++ server/routes/feed.xml.ts
@@ -0,0 +1,27 @@
+import RSS from 'rss';
+import { serverQueryContent } from '#content/server'
+export default defineEventHandler(async (event) => {
+
+  const config = useAppConfig()
+  const docs = await serverQueryContent(event).sort({ date: -1 }).where({ _partial: false }).find()
+  const posts = docs.filter((doc) => doc?._path?.includes('/feed'))
+
+  const feed = new RSS({
+    title: config.info.title,
+    site_url: `${config.info.site_url}`,
+    feed_url: `${config.info.site_url}/feed.xml`,
+  });
+  for (const post of posts) {
+    feed.item({
+      title: post.title ?? '-',
+      url: `${config.info.site_url}/${post._path}`,
+      date: post.date,
+      description: 'asd',
+    });
+  }
+  const feedString = feed.xml({ indent: true });
+
+  event.node.res.setHeader('content-type', 'text/xml');
+  event.node.res.end(feedString);
+
+});
